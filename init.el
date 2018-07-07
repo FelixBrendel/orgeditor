7d053e21a3574f3c7c6fc0440c4230fa680ecb48
FelixBrendel <felix.brendel@freenet.de>
2018-07-07 17:11:39 +0200

    für marcus

--- init.el
+++ init.el
@@ -208,3 +208,11 @@
  '(org-level-1 ((t (:inherit outline-1 :height 1.8))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.3)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (atom-one-dark-theme org-bullets diff-hl tabbar powerline ivy magit use-package))))
